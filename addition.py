98ddd78b6fb4a6fa6b051cd7adc74ac3d8b8dc85
yohani <jeanu28@hotmail.com>
2019-04-08 00:00:15 -0400

    Initial Commit

---
+++ addition.py
@@ -0,0 +1,11 @@
+#Simple Addition program
+
+integer1 = input("Enter first integer:\n") #read string
+integer1 = int( integer1 ) #convert string to integer
+
+integer2 = input("Enter second integer:\n") #read string
+integer2 = int(integer2) #convert string to integer
+
+sum = integer1 + integer2
+
+print ("sum is ", sum)
