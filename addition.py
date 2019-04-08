35b43ef7e9eb5288cb73b8b4c0f7dcf4d6b9082d
yohani <jeanu28@hotmail.com>
2019-04-08 14:35:28 -0400

    Changes to addition.py

--- addition.py
+++ addition.py
@@ -1,11 +1,11 @@
 #Simple Addition program
 
-integer1 = input("Enter first integer:\n") #read string
-integer1 = int( integer1 ) #convert string to integer
+num1 = input("Enter first integer:\n") #read string
+num1 = int( num1 ) #convert string to integer
 
-integer2 = input("Enter second integer:\n") #read string
-integer2 = int(integer2) #convert string to integer
+num2 = input("Enter second integer:\n") #read string
+num2 = int(num2) #convert string to integer
 
-sum = integer1 + integer2
+sum = num1 + num2
 
 print ("sum is ", sum)
